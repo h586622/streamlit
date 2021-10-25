c0d5d29e5242abd673bb1771d4d360a72802579b
h586622 <54099217+h586622@users.noreply.github.com>
2021-10-25 12:39:24 +0200

    Update boxofficedemo.py

--- boxofficedemo.py
+++ boxofficedemo.py
@@ -8,10 +8,10 @@ st.title(" Box Office Predictions ")
 
 st.text_input("Name of movie", "Type here")
 
-genres = st.multiselect('Select genres:', ['Action', 'Comedy', 'Romance', 'Sci-fi'])
+genres = st.multiselect('Select genres:', ['Action', 'Comedy', 'Romance', 'Sci-fi', 'Horror', 'Adventure', 'Documentary'])
 
 budget = st.number_input('Budget', step=100000)
 
-collection = st.radio("Part of a collection: ", ('Yes', 'No'))
+collection = st.checkbox("Part of a collection: ")
 
 production_comp = st.selectbox("Production company", ['Disney', 'None'])
