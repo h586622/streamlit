31090ea483600921534894d67a4f5f621c8adb5c
h586622 <54099217+h586622@users.noreply.github.com>
2021-10-25 10:11:36 +0200

    Update boxofficedemo.py

--- boxofficedemo.py
+++ boxofficedemo.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
-st.write(""" my first app""")
+st.write(""" Box Office Predictions """)
 
 genres = st.multiselect('Select genres:', ['Action', 'Comedy', 'Romance', 'Sci-fi'])
 
-budget = st.number_input('Budget')
+budget = st.number_input('Budget', step=100000)
+
+collection = st.radio("Part of a collection: ", ('Yes', 'No'))
+
+production_comp = st.selectbox("Production company", ['Disney', 'None'])
